73c43233dc47aed8c63e00ea74db29c6902b7d01
mwangipatrick <m271patrick@gmail.com>
2023-03-17 23:52:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-/**
- * Building a multilingual puzzle
- * return :0
- */
-int main(void)
-{
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
-}   
+
