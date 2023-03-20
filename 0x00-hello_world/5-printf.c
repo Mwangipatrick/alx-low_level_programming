8ae49c9222873f3fa32b632dc1c2312b00ba552f
mwangipatrick <m271patrick@gmail.com>
2023-03-20 07:07:44 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - this is the main function for this project 
- * description -  proper grammer outcome is a piece of art
- * return 0 success
- */
+ * main - this is the main function for this project
+ * description -proper grammer outcome is a piece of art
+ *
+ * Return:0 success
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
