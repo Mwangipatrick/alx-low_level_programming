f345d4255500c0af0a581e6e5d863723d5e71e95
mwangipatrick <m271patrick@gmail.com>
2023-03-27 15:50:12 -0700

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void reset_to_98(int *n);
 	void puts_half(char *str);
 	void print_array(int *a, int n);
 	char *_strcpy(char *dest, char *src);
-	void rev_string(char *s)
+	void rev_string(char *s);
+
 
 
