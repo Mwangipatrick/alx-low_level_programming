385ea3397d423ea9ebbce2b4f156b2d6c0d4221d
mwangipatrick <m271patrick@gmail.com>
2023-04-14 15:53:58 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat - function that concatenates two strings
- * @s1:the first string newly allocated memory
- * @s2:the second string null terminated
+ * string_nconcat - function that concatenates two strings.
+ * @s1:the first string
+ * @s2:the second string
  * @n:number of bytes of s2 to be concentrated into s1
- * Return:NULL if it fails.
+ * Return:NULL if it fail
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int w, x, y, z;
+	unsigned int len_s1, len_s2, v, i;
 
 	if (s1 == NULL)
 	{
@@ -20,32 +20,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	w = 0;
-	while (s1[w] != '\0')
+	len_s1 = 0;
+	while (s1[len_s1] != '\0')
 	{
-		w++;
+		len_s1++;
 	}
-	x = 0;
-	while (s2[x] != '\0')
+	len_s2 = 0;
+	while (s2[len_s2] != '\0')
 	{
-		x++;
+		len_s2++;
 	}
-	if (n >= x)
+	if (n >= len_s2)
 	{
-		n = x;
+		n = len_s2;
 	}
-	ptr = (char *)malloc(sizeof(char) * (w + n + 1));
+	ptr = (char *)malloc(sizeof(char) * (len_s1 + n + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (y = 0; y < w; y++)
+	for (h = 0; i < len_s1; i++)
 	{
-		ptr[y] = s1[y];
+		ptr[i] = s1[i];
 	}
-	for (z = 0; z < n; z++)
+	for (v = 0; v < n; v++)
 	{
-		ptr[y + x] = s2{z];
+		ptr[i + v] = s2[v];
 	}
 	ptr[len_s1 + n] = '\0';
 	return (ptr);
