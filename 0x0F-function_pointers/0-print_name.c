e442327b621aac64ba438d32eb7159cc88874d08
mwangipatrick <m271patrick@gmail.com>
2023-04-19 14:58:24 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *print_name- function that prints a name
+ *@name: name to be printed
+ **f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	char *name
+		printf("enter name: ");
+	scanf("%s\n", name);
+	printf("The user entered %s\n", name);
+}
+
+
