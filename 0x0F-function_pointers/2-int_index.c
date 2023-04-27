158f7b7d10e3aa20c8ebcd87a500f43dd7706f1b
Mwangipatrick <m271patrick@gmail.com>
2023-04-27 15:42:48 -0700

    a function that searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+/**
+ *int_index - search for an interger
+ *@arrary:size of characters
+ *@size:size of array 
+ *@cmp:pointer to the function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, res;
+
+	res = -1;
+	if (array && cmp)
+	{
+
+		if (size <= 0)
+		{
+			return (res);
+		}
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			if (cmp(array[i]) > 0)
+			{
+				res = i;
+				break;
+			}
+			if ((cmp(array[i]) == (-1)))
+			{
+				return (res);
+			}
+		}
+
+	}
+	return (res);
+}
