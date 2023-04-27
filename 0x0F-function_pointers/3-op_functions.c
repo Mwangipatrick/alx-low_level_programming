bb5e384611383ccfbadc578497694afd3f13d250
Mwangipatrick <m271patrick@gmail.com>
2023-04-27 16:02:12 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - finds the sum
+ * @a:the first no.
+ * @b:the second no.
+ * Return:sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *  op_sub - function finds the difference
+ *  @a:the first letter
+ *  @b:the second letter
+ *  Return:difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul -function finds the product
+ * @a:the first letter
+ * @b:the second letter
+ * Return:product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div -function finds the division
+ * @a:the first number
+ * @b:the second number
+ * Return:division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - finds remainder
+ * @a:the number
+ * @b:the divisor
+ * Return:remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
