e23dd3237fff2311efdc5133c6629438ba22a6ce
Mwangipatrick <m271patrick@gmail.com>
2023-05-03 17:33:39 -0700

    a afunction that frees

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - function frees in a loop
+ * @head: pointer to the head 
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
